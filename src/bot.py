2bc60963272b03fb8388f55f7c1b5dabcff3722d
Alexey Larin <102014897+larrriiin@users.noreply.github.com>
2023-03-22 11:30:52 +0300

    Update 0.101
    Добавлены кнопки для кастомизации персонажа (правда пока что нихуя не работают), немного упрощён код для кастомизации имени

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,6 @@ import logging
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     ApplicationBuilder,
-    Application,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
@@ -14,6 +13,8 @@ import sqlite3
 from sqlite3 import Error
 
 CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+CHOOSING_AVATAR, TYPING_HAIR, TYPING_FACE, TYPING_BODY = range(4)
+
 
 def create_connection(path):
     connection = None
@@ -25,6 +26,7 @@ def create_connection(path):
 
     return connection
 
+
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
@@ -34,13 +36,16 @@ def execute_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Добро пожаловать в Team Builder Bot! Введите /help, чтобы просмотреть доступные команды.")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="Добро пожаловать в Team Builder Bot! Введите /help, чтобы просмотреть доступные команды.")
     message = update.message
     user = message.from_user
     user_id = message.from_user.id
@@ -54,7 +59,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     VALUES
       ('{user_id}', '{username}');
     """
-    con = create_connection('../db/database.db') # perhaps здесь переебет, не уверен что так можно... (ненавижу питон)
+    con = create_connection('../db/database.db')  # perhaps здесь переебет, не уверен что так можно... (ненавижу питон)
     # ну кстати, не переебало
     create_users_table = """
     CREATE TABLE IF NOT EXISTS users (
@@ -66,18 +71,20 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     execute_query(con, create_users_table)
     execute_query(con, create_users)
     con.close()
-    
-async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-     message = "Вот доступные команды:\n\n" \
+
+
+async def help_me(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    message = "Вот доступные команды:\n\n" \
               "/start - Начало использования бота\n" \
               "/help - Просмотр доступных команд\n" \
               "/custom - Настройка вашего игрового персонажа\n" \
               "/upgrade - Улучшайте своего персонажа с помощью реальных событий или внутриигровыми способами\n" \
               "/fight - Сражайтесь с другими игроками в чате"
-     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-     
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+
 async def custom(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    reply_keyboard = [["Изменить имя", "Изменить аватара"], ["Назад"]]
+    reply_keyboard = [["Изменить имя", "Изменить аватара"], ["Отмена"]]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
     await update.message.reply_text(
         "Что бы Вы хотели изменить?",
@@ -85,19 +92,48 @@ async def custom(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
     return CHOOSING
 
-async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def custom_name_choice(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
     context.user_data["choice"] = text
     if text == "Изменить имя":
         await update.message.reply_text("Введите новое имя:")
         return TYPING_REPLY
+
+
+async def custom_avatar(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_keyboard = [["Изменить волосы", "Изменить лицо", "Изменить тело"], ["Назад"]]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    await update.message.reply_text(
+        "Что бы Вы хотели изменить в аватаре?",
+        reply_markup=markup,
+    )
+    return CHOOSING_AVATAR
+
+
+async def custom_avatar_choice(update: Update, context: ContextTypes.DEFAULT_TYPE):  # Не работает
+    text = update.message.text
+    context.user_data["choice"] = text
+    if text == "Изменить волосы":
+        # Add functionality to select hair image
+        await update.message.reply_text("Изменяем волосы...")
+        return TYPING_HAIR
+    elif text == "Изменить лицо":
+        # Add functionality to select face image
+        await update.message.reply_text("Изменяем лицо...")
+        return TYPING_FACE
+    elif text == "Изменить тело":
+        # Add functionality to select body image
+        await update.message.reply_text("Изменяем тело...")
+        return TYPING_BODY
     elif text == "Назад":
-        return custom_cancel
+        return CHOOSING
     else:
         await update.message.reply_text("Я не понимаю, что вы хотите. Пожалуйста, выберите из меню.")
-        return CHOOSING
+        return TYPING_CHOICE
 
-async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+async def received_name(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
     context.user_data["name"] = text
     user_id = update.message.from_user.id
@@ -112,50 +148,53 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
     await update.message.reply_text(f"Имя изменено на {text}.")
     return ConversationHandler.END
 
+
 async def custom_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Действие отменено.", reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
+
 async def upgrade(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = "Чтобы улучшить своего персонажа, участвуйте в реальных событиях или выполняйте внутриигровые задания. Более подробная информация скоро появится!"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
-    
+
+
 async def fight(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = "Чтобы сразиться с другими игроками, отправьте мне прямое сообщение с именем пользователя вашего противника и начинайте битву! Более подробная информация скоро появится."
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
 
 if __name__ == '__main__':
     token_file = open("../tokens/tg_app_token.txt", "r")
     application = ApplicationBuilder().token(token_file.read()).build()
     token_file.close()
     application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('help', help))
+    application.add_handler(CommandHandler('help', help_me))
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("custom", custom)],
         states={
             CHOOSING: [
-                MessageHandler(
-                    filters.Regex("^(Изменить имя|Изменить Аватара)$"), regular_choice
-                ),
+                MessageHandler(filters.Regex("^Изменить имя$"), custom_name_choice),
+                MessageHandler(filters.Regex('^Изменить аватара$'), custom_avatar),
             ],
-            TYPING_CHOICE: [
-                MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Назад$")), regular_choice # нихуя не работает
-                )
-            ],
+            TYPING_CHOICE: [MessageHandler(filters=filters.Regex('^Изменить волосы$'),  # Пока что не работает
+                                           callback=custom_avatar_choice),
+                            MessageHandler(filters=filters.Regex('^Изменить лицо$'),
+                                           callback=custom_avatar_choice),
+                            MessageHandler(filters=filters.Regex('^Изменить тело$'),
+                                           callback=custom_avatar_choice)],
             TYPING_REPLY: [
                 MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Назад$")), # как и это
-                    received_information,
-                )
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Назад$")), received_name, )
             ],
         },
-        fallbacks=[MessageHandler(filters.Regex("^Назад$"), custom_cancel)], # и это
+        fallbacks=[MessageHandler(filters.Regex("^Отмена$"), custom_cancel)],
     )
-
-    application.add_handler(conv_handler) # зато в общем работает
+    back_to_custom_handler = MessageHandler(filters=filters.Regex("Назад"), callback=custom)
+    application.add_handler(back_to_custom_handler, group=1)
+    application.add_handler(conv_handler)
 
     application.add_handler(CommandHandler('upgrade', upgrade))
     application.add_handler(CommandHandler('fight', fight))
-    
+
     application.run_polling()
