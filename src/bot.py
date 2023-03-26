6897e44da52339b36259f9cd893b2fdb13077b39
Alexey Larin <102014897+larrriiin@users.noreply.github.com>
2023-03-26 13:01:16 +0300

    Update 0.201
    added some functions

--- src/bot.py
+++ src/bot.py
@@ -19,7 +19,9 @@ CLASS_CHOOSING, SUBMIT_CLASS, WHERE_CHOOSING = range(3)
 
 
 async def del_keyboard(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text='Клавиатура удалена! (Юра пидор)', reply_markup=ReplyKeyboardRemove())
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='Клавиатура удалена! (Юра пидор)',
+                                   reply_markup=ReplyKeyboardRemove())
+
 
 async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     con = create_connection('../db/database.db')
@@ -28,7 +30,8 @@ async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_class = execute_read_query(con, request)
     con.close()
     if user_class[0][0] is None:
-        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=open(os.path.abspath('../res/locations/gate.png'), 'rb'),
+        await context.bot.send_photo(chat_id=update.effective_chat.id,
+                                     photo=open(os.path.abspath('../res/locations/gate.png'), 'rb'),
                                      caption="Добро пожаловать в Великую Империю. Ее выбрали вы, или ее выбрали за вас" \
                                              " — это лучшее государство из оставшихся.\n\n Сейчас Вам нужно получить лицензию на роль класса.")
         classes_description = "Доступные классы:\n\n" \
@@ -74,7 +77,39 @@ async def assignments(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = 'Вы подходите к небольшому зданию с вывеской "Дом поручений". ' \
               'При входе Вы замечаете, что на стенах висят доски объявлений с различными заданиями.\n' \
               'Может и получится найти, что-то Вам по душе...'
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)  # Здесь будут задания
+
+
+async def chronos(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not is_available(update.message.from_user.id, 500):
+        message = 'Вы подходите к огромному храму, но какая-то неведомая сила не даёт Вам пройти внутрь.\nВозможно, Вам пока что не хватает опыта.'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        pass  # Здесь будет прокачка героя
+
+
+async def lab(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not is_available(update.message.from_user.id, 500):
+        message = 'Вы приходите к лаборатории, но дверь оказывается закрытой.\nВозможно, Вам пока что не хватает опыта.'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        pass  # Здесь можно будет крафтить расходники
+
+
+async def guild_house(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not is_available(update.message.from_user.id, 1000):
+        message = 'Вы приходите к дому гильдий. По крайней мере так сказал стражник...\nОн не пропускает Вас под предлогом, что Вы недостаточно опытны.'
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+    else:
+        pass  # Здесь можно будет запрашивать ресурсы
+
+
+def is_available(user_id, required_exp):
+    con = create_connection('../db/database.db')
+    request = f"SELECT exp FROM users WHERE id={user_id}"
+    user_exp = execute_read_query(con, request)
+    con.close()
+    return user_exp[0][0] >= required_exp
 
 
 async def game_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -95,6 +130,9 @@ if __name__ == '__main__':
             ],
             WHERE_CHOOSING: [
                 MessageHandler(filters.Regex("^Дом поручений$"), assignments),
+                MessageHandler(filters.Regex("^Храм Хроноса$"), chronos),
+                MessageHandler(filters.Regex("^Лаборатория$"), lab),
+                MessageHandler(filters.Regex("^Дом гильдий$"), guild_house),
                 #  Сюда добавим все остальные локации
             ],
         },
