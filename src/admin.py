bad40cca26c4b798577083971fe5110f47871eef
Alexey Larin <102014897+larrriiin@users.noreply.github.com>
2023-03-29 20:47:45 +0300

    Update 0.212
    Поправлен вывод сообщения при добавлении эвента
    Изменено название переменной (Юра, пора ходить на английский)
    Теперь добавление эвента пока не будет добавлено или пока не нажата  кнопка "Назад"

--- src/admin.py
+++ src/admin.py
@@ -37,39 +37,42 @@ async def admin_cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def add_event(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    msg = "Вы собираетесь добавить новое глобальное событие\n\n \
-          Обратите внимание, что информацию о новом событии необходимо вводить СТРОГО в указанном ниже формате\n \
-          Все поля должны быть разделены знаком переноса строки (через Shift+Enter).\n\n \
-          Формат информации о событии: \n \
-          Название события (не более 50 символов)\n \
-          Описание события (не более 500 символов)\n \
-          Время начала события в формате yyyy-MM-dd hh:mm:ss\n \
-          Продолжительность события (целое число, в минутах)\n \
-          Награда опытом за событие (целое число >=0)\n\n\n \
-          Пример ввода информации для нового события: \n \
-          Мое новое событие\n \
-          Это событие будет лучшим в истории!\n \
-          2027-12-12 23:59:59\n \
-          60\n \
-          365\n\n\n \
-          Теперь введите информацию о новом событии в ответном сообщении."
+    msg = "Вы собираетесь добавить новое глобальное событие\n\n" \
+          "Обратите внимание, что информацию о новом событии необходимо вводить СТРОГО в указанном ниже формате\n" \
+          "Все поля должны быть разделены знаком переноса строки (через Shift+Enter).\n\n" \
+          "Формат информации о событии: \n" \
+          "Название события (не более 50 символов)\n" \
+          "Описание события (не более 500 символов)\n" \
+          "Время начала события в формате yyyy-MM-dd hh:mm:ss\n" \
+          "Продолжительность события (целое число, в минутах)\n" \
+          "Награда опытом за событие (целое число >=0)\n\n\n" \
+          "Пример ввода информации для нового события: \n" \
+          "Мое новое событие\n" \
+          "Это событие будет лучшим в истории!\n" \
+          "2027-12-12 23:59:59\n" \
+          "60\n" \
+          "365\n\n\n" \
+          "Теперь введите информацию о новом событии в ответном сообщении."
     await update.message.reply_text(msg)
     return EVENT_INPUT
 
 
 async def event_input(update: Update, context: ContextTypes.DEFAULT_TYPE):
     admin_keyboard = [["Добавить событие", "Добавить администратора"], ["Отмена"]]
-    markup = ReplyKeyboardMarkup(admin_keyboard, one_time_keyboard=True)
+    cancel_keyboard = [["Назад"]]
     text = update.message.text
     is_ok, msg = parse_new_event_info_string(text)
-    responce = "Создание нового события " + "прошло успешно.\n" if is_ok else "провалилось.\n" \
-                + "Комментарий: " + msg
     if is_ok:
+        response = f"Создание нового события прошло успешно!\nКомментарий: {msg}"
         save_new_event_info_string_to_db(text)
-    
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=responce, reply_markup=markup)
-
-    return ADMIN_CHOOSING
+        markup = ReplyKeyboardMarkup(admin_keyboard, one_time_keyboard=True)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=response, reply_markup=markup)
+        return ADMIN_CHOOSING
+    else:
+        markup = ReplyKeyboardMarkup(cancel_keyboard, one_time_keyboard=True)
+        response = f"Ошибка при создании события!\nКомментарий: {msg}\n\nПопробуйте ещё раз."
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=response, reply_markup=markup)
+        return EVENT_INPUT
 
 
 async def op(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -113,7 +116,8 @@ admin_handler = ConversationHandler(
         ],
         EVENT_INPUT: [
             MessageHandler(
-                filters.TEXT & ~(filters.COMMAND | filters.Regex("^Отмена$")), event_input)
+                filters.TEXT & ~(filters.COMMAND | filters.Regex("^Отмена$|^Назад$")), event_input),
+            MessageHandler(filters.Regex("^Назад$"), admin),
         ]
     },
     fallbacks=[MessageHandler(filters.Regex("^Отмена$"), admin_cancel)],
@@ -126,8 +130,8 @@ def parse_new_event_info_string(text):
 
     if len(fields) != 5:
         return False, 'Вы указали недостаточно информации о новом событии. ' \
-                       'Возможно, вы забыли разделить информацию с помощью Shift+Enter, либо пропустили какое-то из '\
-                       'полей.'
+                      'Возможно, вы забыли разделить информацию с помощью Shift+Enter, либо пропустили какое-то из ' \
+                      'полей.'
 
     name = fields[0]
     if len(name) > 50:
@@ -140,7 +144,7 @@ def parse_new_event_info_string(text):
     start_time = fields[2]
     if not ensure_time_format(start_time):
         return False, 'Указанное вами время события не прошло проверку на правильность формата.' \
-                       'Не забывайте, что формат должен СТРОГО соответствовать следующему формату: yyyy-MM-dd hh:mm:ss'
+                      'Не забывайте, что формат должен СТРОГО соответствовать следующему формату: yyyy-MM-dd hh:mm:ss'
 
     duration = fields[3]
     if not duration.isdigit():
@@ -148,7 +152,7 @@ def parse_new_event_info_string(text):
     if int(duration) < 1:
         return False, 'Продолжительность события не может быть меньше 1 минуты.'
     if int(duration) > mins_in_year:
-        return False, f"""Продолжительность события не может быть больше года ('{mins_in_year}' минут)."""
+        return False, f"Продолжительность события не может быть больше года ('{mins_in_year}' минут)."
 
     exp_reward = fields[4]
     if not exp_reward.isdigit():
