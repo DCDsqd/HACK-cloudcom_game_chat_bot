d8a11f1a91b5261f366a519fa3761503f0ebb6ca
storst1 <kokakolazzz@mail.ru>
2023-03-29 13:46:33 +0300

    Update ```sql``` scripts so they are up to date

--- sql/database_create_tables.sql
+++ sql/database_create_tables.sql
@@ -1,25 +1,25 @@
-CREATE TABLE IF NOT EXISTS "users" (
+CREATE TABLE "users" (
 	"id"	INTEGER UNIQUE,
 	"username"	TEXT NOT NULL UNIQUE,
 	"personal_username"	TEXT UNIQUE,
 	"game_class"	TEXT,
-	"game_subclass" TEXT NOT NULL DEFAULT 'None',
+	"game_subclass"	TEXT NOT NULL DEFAULT 'None',
 	"exp"	INTEGER NOT NULL DEFAULT 0,
-	"rank"	TEXT NOT NULL DEFAULT 'Новичок',
 	"hair_id"	INTEGER NOT NULL DEFAULT 1,
-	"face_id"	INTEGER NOT NULL DEFAULT 1,
+	"face_id"	TEXT NOT NULL DEFAULT 1,
 	"shoulders_id"	INTEGER NOT NULL DEFAULT 1,
 	"admin"	INTEGER NOT NULL DEFAULT 0,
 	PRIMARY KEY("id" AUTOINCREMENT)
-)
+);
 
-CREATE TABLE IF NOT EXISTS "global_events" (
+CREATE TABLE "global_events" (
 	"id"	INTEGER UNIQUE,
 	"name"	TEXT NOT NULL UNIQUE,
 	"descr"	TEXT NOT NULL CHECK(length("descr") < 1000),
 	"start_time"	TEXT NOT NULL,
 	"duration"	INTEGER NOT NULL DEFAULT 5,
 	"participants"	TEXT,
+	"exp_reward"	INTEGER DEFAULT 0,
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
