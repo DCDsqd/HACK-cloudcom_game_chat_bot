f1c680abd0d2b9baef453c22e44260a10860ff01
Yuriy Vinnik <89082426+storst1@users.noreply.github.com>
2023-04-10 23:32:04 +0300

    Update menu_chain.py

--- src/menu_chain.py
+++ src/menu_chain.py
@@ -4,7 +4,7 @@ from telegram.ext import (
     ConversationHandler,
 )
 
-menu_keyboard = [['Кастомизация', 'Игра'], ['Добавить эвент', 'Друзья']]
+menu_keyboard = [['Кастомизация', 'Игра'], ['Добавить событие', 'Друзья']]
 
 
 async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
