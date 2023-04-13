f1cfbfb9826630b36638fe678bf0de8c99057550
Yuriy Vinnik <89082426+storst1@users.noreply.github.com>
2023-04-13 07:27:00 +0300

    Update run_lupdate_for_admin_tool_from_external_lupdate.sh

--- scripts/run_lupdate_for_admin_tool_from_external_lupdate.sh
+++ scripts/run_lupdate_for_admin_tool_from_external_lupdate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# This script relies on the fact that utils/external/lupdate folder has lupdate.exe executable (in a working state)
-
 cd ../utils/external/lupdate
 lupdate "../../admin_tool/game-chat-admin-tool/game-chat-admin-tool.pro"
 $SHELL
+
+
+# !!! This script relies on the fact that utils/external/lupdate folder has lupdate.exe executable (in a working state)
