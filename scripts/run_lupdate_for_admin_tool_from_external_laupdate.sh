c43ceee3cee925b2639356f3c9b2820717d50401
Yuriy Vinnik <89082426+storst1@users.noreply.github.com>
2023-04-13 03:36:35 +0300

    Create run_lupdate_for_admin_tool_from_external_laupdate.sh

---
+++ scripts/run_lupdate_for_admin_tool_from_external_laupdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script relies on the fact that utils/external/lupdate folder has lupdate.exe executable (in a working state)
+
+cd ../utils/external/lupdate
+lupdate "../../admin_tool/game-chat-admin-tool/game-chat-admin-tool.pro"
+$SHELL
