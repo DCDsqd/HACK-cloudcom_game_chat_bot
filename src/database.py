0e54be569ddc7b96552f46f2f363855107a19b53
storst1 <kokakolazzz100@mail.ru>
2023-04-27 03:47:00 +0300

    Tabulation fix

--- src/database.py
+++ src/database.py
@@ -1124,10 +1124,10 @@ class Database:
         execute_query(self.database_conn, create_armor_query)
 
         meta_armor_id_query = f"""
-                            SELECT meta_item_id FROM items_owned
-                            WHERE base_item_id = {base_armor_id}
-                            AND owner_id = {user_id};
-                         """
+                                SELECT meta_item_id FROM items_owned
+                                WHERE base_item_id = {base_armor_id}
+                                AND owner_id = {user_id};
+                             """
         meta_armor_id = execute_read_query(self.database_conn, meta_armor_id_query)[0][0]
 
         create_weapon_query = f"""
